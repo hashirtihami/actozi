00f8497646a8a0dd3a7346b3145f43e20d841781
unknown <tihaami>
2019-06-21 16:44:08 +0500

    db shit

--- server.js
+++ server.js
@@ -9,3 +9,21 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
   console.log(`App is up and running. Listening on port ${PORT}`);
 });
+
+// var mysql = require('mysql')
+// var connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   password: 'password',
+//   database: 'my_db'
+// })
+
+// connection.connect()
+
+// connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
+//   if (err) throw err
+
+//   console.log('The solution is: ', rows[0].solution)
+// })
+
+// connection.end()
