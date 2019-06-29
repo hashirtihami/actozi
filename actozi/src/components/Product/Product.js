63be10614df32a3f7ae3d2b0b4c5bf86e8549ac5
wahaj-47 <wahajhssn@gmail.com>
2019-06-29 17:30:57 +0500

    CSS ANIMATIONS git add -A!

--- actozi/src/components/Product/Product.js
+++ actozi/src/components/Product/Product.js
@@ -10,16 +10,16 @@ export default class Product extends React.Component {
       <div className="productContainer">
         <div>
           <Image
-            className="image"
+            className="productImage"
             fluid
             src="https://scontent.fkhi8-1.fna.fbcdn.net/v/t1.0-9/64443102_1597979237000345_6578358883364896768_n.jpg?_nc_cat=100&_nc_oc=AQkgAuPSSIQ-MBN48c7_YvbLMxZU4FIRhAX8RC9tC5G53ce8jrx36JnSC3CkmbUzH_Y&_nc_ht=scontent.fkhi8-1.fna&oh=187a77e359d5cf6e67d63228615f9937&oe=5DB4AA1D"
           />
         </div>
         <div className="productInfo">
-          <a className="title">
+          <a className="productTitle">
             <span>Captain America Tshirt</span>
           </a>
-          <div className="price">
+          <div className="productPrice">
             <span>Rs 900/-</span>
           </div>
         </div>
