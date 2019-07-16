7a2a1aae1e3f4476f148f564cc1130fd25e5edec
wahaj-47 <wahajhssn@gmail.com>
2019-07-16 15:37:17 +0500

    Displaying products only in Database

--- actozi/src/components/ProductTile/ProductTile.js
+++ actozi/src/components/ProductTile/ProductTile.js
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios";
 
 import Image from "react-bootstrap/Image";
 
@@ -9,18 +10,14 @@ export default class ProductTile extends React.Component {
     return (
       <div className="productContainer">
         <div>
-          <Image
-            className="productImage"
-            fluid
-            src="https://scontent.fkhi8-1.fna.fbcdn.net/v/t1.0-9/64443102_1597979237000345_6578358883364896768_n.jpg?_nc_cat=100&_nc_oc=AQkgAuPSSIQ-MBN48c7_YvbLMxZU4FIRhAX8RC9tC5G53ce8jrx36JnSC3CkmbUzH_Y&_nc_ht=scontent.fkhi8-1.fna&oh=187a77e359d5cf6e67d63228615f9937&oe=5DB4AA1D"
-          />
+          <Image className="productImage" fluid src={this.props.url} />
         </div>
         <div className="productInfo">
           <a className="productTitle">
-            <span>Captain America Tshirt</span>
+            <span>{this.props.name}</span>
           </a>
           <div className="productPrice">
-            <span>Rs 900/-</span>
+            <span>{this.props.price}/-</span>
           </div>
         </div>
       </div>
--- actozi/src/screens/Home/Home.js
+++ actozi/src/screens/Home/Home.js
@@ -1,4 +1,6 @@
 import React from "react";
+import axios from "axios";
+
 // import logo from "./logo.svg";
 import "./Home.css";
 import "bootstrap/dist/css/bootstrap.css";
@@ -12,54 +14,74 @@ import Footer from "../../components/Footer/Footer";
 import Tile from "../../components/Tile/Tile";
 import ProductTile from "../../components/ProductTile/ProductTile";
 
-function Home() {
-  return (
-    <Container fluid>
-      <Row>
-        <Col lg={12} className="no-padding">
-          <Header />
-        </Col>
-      </Row>
-      <Row>
-        <Col lg={12} className="no-padding">
-          <Carousel />
-        </Col>
-      </Row>
-      <Row className="margin-top">
-        <Col lg={4} sm={4} xs={4}>
-          <Tile />
-        </Col>
-        <Col lg={4} sm={4} xs={4}>
-          <Tile />
-        </Col>
-        <Col lg={4} sm={4} xs={4}>
-          <Tile />
-        </Col>
-      </Row>
-      <Row className="featured margin-top">
-        <h1>Featured Products</h1>
-      </Row>
-      <Row className="grid">
-        <Col md lg>
-          <ProductTile />
-        </Col>
-        <Col md lg>
-          <ProductTile />
-        </Col>
-        <Col md lg>
-          <ProductTile />
-        </Col>
-        <Col md lg>
-          <ProductTile />
-        </Col>
-      </Row>
-      <Row>
-        <Col lg={12} className="no-padding">
-          <Footer />
-        </Col>
-      </Row>
-    </Container>
-  );
+class Home extends React.Component {
+  state = {
+    products: []
+  };
+
+  componentDidMount() {
+    axios.get("/api/products/").then(response => {
+      this.setState({ products: response.data.data });
+    });
+  }
+
+  createProductTile = product => {
+    console.log(product);
+    return (
+      <Col md lg key={product.productId}>
+        <ProductTile
+          category={product.category}
+          name={product.name}
+          desc={product.desc}
+          url={product.url}
+          price={product.price}
+        />
+      </Col>
+    );
+  };
+
+  render() {
+    return (
+      <Container fluid>
+        <Row>
+          <Col lg={12} className="no-padding">
+            <Header />
+          </Col>
+        </Row>
+        <Row>
+          <Col lg={12} className="no-padding">
+            <Carousel />
+          </Col>
+        </Row>
+        <Row className="margin-top">
+          <Col lg={4} sm={4} xs={4}>
+            <Tile />
+          </Col>
+          <Col lg={4} sm={4} xs={4}>
+            <Tile />
+          </Col>
+          <Col lg={4} sm={4} xs={4}>
+            <Tile />
+          </Col>
+        </Row>
+        <Row className="featured margin-top">
+          <h1>Featured Products</h1>
+        </Row>
+        <Row className="grid">
+          {this.state.products.length !== 0 ? (
+            this.state.products.map(product => this.createProductTile(product))
+          ) : (
+            <div>Loading</div>
+          )}
+        </Row>
+        <Row>
+          <Col lg={12} className="no-padding">
+            <Footer />
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
 }
 
 export default Home;
